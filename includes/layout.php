c9a7790ddd23a6a6060119d3facede97225bbbc2
Nipesh001 <nishantgiri193@gmail.com>
2025-04-11 14:44:25 +0545

    favicon fixed

--- includes/layout.php
+++ includes/layout.php
@@ -10,7 +10,8 @@ function renderHeader()
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Premium Cinema Experience</title>
+    <title>Premium Cinema</title>
+    <link rel="icon" type="image/png" href="./img/logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
     <style>
